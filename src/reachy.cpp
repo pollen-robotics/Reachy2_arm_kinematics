621d0ee363dc3903ffd8ebc8d2a4920f14cb2ecf
glannuzel <gaelle.lannuzel@gmail.com>
2024-08-30 12:02:05 +0200

    Change kdl chain

--- src/reachy.cpp
+++ src/reachy.cpp
@@ -6,21 +6,24 @@ namespace KDL{
     Chain Reachy_RightArm(){
         Chain reachy;
 
+        // Convert angles to radians
+        double roll_rad = 10.0 * M_PI / 180.0; // -15.0 * M_PI / 180.0;
+        double pitch_rad = 0.0;
+        double yaw_rad = 15.0 * M_PI / 180.0; // 10.0 * M_PI / 180.0;
+
+        // Create the rotation matrix from RPY angles
+        Rotation rpy_rotation = Rotation::RPY(roll_rad, pitch_rad, yaw_rad);
+
         // r_shoulder_pitch
-        double offset_pitch_rad = -15.0 * M_PI / 180.0;
-        reachy.addSegment(Segment(Joint(Vector(0.0, -0.20, 0.0), Vector(0.0, 1.0, 0.0), Joint::RotAxis),
-                                   Frame(Rotation::RotY(offset_pitch_rad)) * 
-                                   Frame(Rotation(1.0, 0.0, 0.0, 0.0, 1.0, 0.0 , 0.0, 0.0, 1.0), 
-                                             Vector(0.0, -0.20, 0.0))
-                                   ));
+        reachy.addSegment(Segment(Joint(Vector(0.0, -0.20, 0.0), rpy_rotation * Vector(0.0, 1.0, 0.0), Joint::RotAxis),
+                                   Frame(rpy_rotation, 
+                                   Vector(0.0, -0.20, 0.0))));
         // r_shoulder_roll
         reachy.addSegment(Segment(Joint(Vector(0.0, 0.0, 0.0), Vector(1.0, 0.0, 0.0), Joint::RotAxis),
                                    Frame(Rotation(1.0, 0.0, 0.0, 0.0, 1.0, 0.0 , 0.0, 0.0, 1.0), 
                                         Vector(0.0, 0.0, 0.0))));
         // r_arm_yaw
-        double offset_yaw_rad = 10.0 * M_PI / 180.0;
         reachy.addSegment(Segment(Joint(Vector(0.0, 0.0, -0.28), Vector(0.0, 0.0, 1.0), Joint::RotAxis),
-                                   Frame(Rotation::RotZ(offset_yaw_rad)) * 
                                    Frame(Rotation(1.0, 0.0, 0.0, 0.0, 1.0, 0.0 , 0.0, 0.0, 1.0), 
                                         Vector(0.0, 0.0, -0.28))
                                    ));
@@ -50,21 +53,25 @@ namespace KDL{
 
     Chain Reachy_LeftArm(){
         Chain reachy;
+
+        // Convert angles to radians
+        double roll_rad = -10.0 * M_PI / 180.0; // 15.0 * M_PI / 180.0;
+        double pitch_rad = 0.0;
+        double yaw_rad = -15.0 * M_PI / 180.0; // -10.0 * M_PI / 180.0;
+
+        // Create the rotation matrix from RPY angles
+        Rotation rpy_rotation = Rotation::RPY(roll_rad, pitch_rad, yaw_rad);
+
         // l_shoulder_pitch
-        double offset_pitch_rad = 15.0 * M_PI / 180.0;
-        reachy.addSegment(Segment(Joint(Vector(0.0, 0.20, 0.0), Vector(0.0, 1.0, 0.0), Joint::RotAxis),
-                                   Frame(Rotation::RotY(offset_pitch_rad)) * 
-                                   Frame(Rotation(1.0, 0.0, 0.0, 0.0, 1.0, 0.0 , 0.0, 0.0, 1.0), 
-                                             Vector(0.0, -0.20, 0.0))
-                                   ));
+        reachy.addSegment(Segment(Joint(Vector(0.0, 0.20, 0.0), rpy_rotation * Vector(0.0, 1.0, 0.0), Joint::RotAxis),
+                                   Frame(rpy_rotation, 
+                                   Vector(0.0, 0.20, 0.0))));
         // l_shoulder_roll
         reachy.addSegment(Segment(Joint(Vector(0.0, 0.0, 0.0), Vector(1.0, 0.0, 0.0), Joint::RotAxis),
                                    Frame(Rotation(1.0, 0.0, 0.0, 0.0, 1.0, 0.0 , 0.0, 0.0, 1.0), 
                                         Vector(0.0, 0.0, 0.0))));
         // l_arm_yaw
-        double offset_yaw_rad = 10.0 * M_PI / 180.0;
         reachy.addSegment(Segment(Joint(Vector(0.0, 0.0, -0.28), Vector(0.0, 0.0, 1.0), Joint::RotAxis),
-                                   Frame(Rotation::RotZ(offset_yaw_rad)) * 
                                    Frame(Rotation(1.0, 0.0, 0.0, 0.0, 1.0, 0.0 , 0.0, 0.0, 1.0), 
                                         Vector(0.0, 0.0, -0.28))
                                    ));
